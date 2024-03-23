b1ccb32a976d4d02e585cff1637226aaada51351
aliyeff-f <eliyevnurlan811@gmail.com>
2024-03-23 19:51:02 +0400

    main

--- Fitlife/assets/js/script.js
+++ Fitlife/assets/js/script.js
@@ -1,11 +1,6 @@
 'use strict';
 
 
-
-/**
- * add event on element
- */
-
 const addEventOnElem = function (elem, type, callback) {
   if (elem.length > 1) {
     for (let i = 0; i < elem.length; i++) {
@@ -15,12 +10,6 @@ const addEventOnElem = function (elem, type, callback) {
     elem.addEventListener(type, callback);
   }
 }
-
-
-
-/**
- * navbar toggle
- */
 
 const navbar = document.querySelector("[data-navbar]");
 const navTogglers = document.querySelectorAll("[data-nav-toggler]");
